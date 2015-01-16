53bf321788f06908811c33f18bc2546de48bd178
Adrian Salido-Moreno <adrianm@codeaurora.org>
2015-01-16 12:08:32 -0800

    msm: mdss: remove downscale overflow check for recent MDP revisions
    The overflow check for downscaling was added due to bug in scaler for
    earlier MDP revisions. The bug has been addressed in MDP 1.03 and newer
    revisions, hence keep check only for older revisions.
    
    Change-Id: I2df98f7d23c016472bc4bcf67ce37fba4fb2c40e
    Signed-off-by: Adrian Salido-Moreno <adrianm@codeaurora.org>
    Signed-off-by: Krishna Chaitanya Devarakonda <kdevarak@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/video/msm/mdss/mdss_mdp_util.c
+++ drivers/video/msm/mdss/mdss_mdp_util.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -621,6 +621,7 @@ int mdss_mdp_get_img(struct msmfb_data *img, struct mdss_mdp_img_data *data)
 int mdss_mdp_calc_phase_step(u32 src, u32 dst, u32 *out_phase)
 {
 	u32 unit, residue, result;
+	struct mdss_data_type *mdata = mdss_mdp_get_mdata();
 
 	if (src == 0 || dst == 0)
 		return -EINVAL;
@@ -629,7 +630,7 @@ int mdss_mdp_calc_phase_step(u32 src, u32 dst, u32 *out_phase)
 	*out_phase = mult_frac(unit, src, dst);
 
 	/* check if overflow is possible */
-	if (src > dst) {
+	if ((mdata->mdp_rev < MDSS_MDP_HW_REV_103) && src > dst) {
 		residue = *out_phase - unit;
 		result = (residue * dst) + residue;
 
