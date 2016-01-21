62940ebf2a29a5a63ba81c214d8ae1f12e009b1f
desalesouche <desalesouche@gmail.com>
2016-01-21 03:19:12 +0100

    lib/string: use glibc version
    the performance of memcpy and memmove of the general version is very
    inefficient, this patch improved them.
    
    Signed-off-by: Miao Xie <miaox*******>
    Signed-off-by: engstk <eng.stk@sapo.pt>
    
    Conflicts:
    
    	lib/string.c
    Signed-off-by: flar2 <asegaert@gmail.com>
    
    Change-Id: I83e80d10f38be2e3b26c6078c276ec075de5fc1c
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- lib/string.c
+++ lib/string.c
@@ -27,6 +27,7 @@
 #include <linux/bug.h>
 #include <linux/errno.h>
 #include <linux/linkage.h>
+#include <linux/memcopy.h>
 
 #ifndef __HAVE_ARCH_STRNICMP
 /**
@@ -750,11 +751,11 @@ EXPORT_SYMBOL(memzero_explicit);
  */
 void *memcpy(void *dest, const void *src, size_t count)
 {
-	char *tmp = dest;
-	const char *s = src;
+	unsigned long dstp = (unsigned long)dest; 
+	unsigned long srcp = (unsigned long)src; 
 
-	while (count--)
-		*tmp++ = *s++;
+	/* Copy from the beginning to the end */ 
+	mem_copy_fwd(dstp, srcp, count); 
 	return dest;
 }
 EXPORT_SYMBOL(memcpy);
@@ -771,21 +772,15 @@ EXPORT_SYMBOL(memcpy);
  */
 void *memmove(void *dest, const void *src, size_t count)
 {
-	char *tmp;
-	const char *s;
+	unsigned long dstp = (unsigned long)dest; 
+	unsigned long srcp = (unsigned long)src; 
 
-	if (dest <= src) {
-		tmp = dest;
-		s = src;
-		while (count--)
-			*tmp++ = *s++;
+	if (dest - src >= count) { 
+		/* Copy from the beginning to the end */ 
+		mem_copy_fwd(dstp, srcp, count); 
 	} else {
-		tmp = dest;
-		tmp += count;
-		s = src;
-		s += count;
-		while (count--)
-			*--tmp = *--s;
+		/* Copy from the end to the beginning */ 
+		mem_copy_bwd(dstp, srcp, count); 
 	}
 	return dest;
 }
